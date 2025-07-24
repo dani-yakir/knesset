bbab27f4e7a8d0b3eecf4bdf12320895e2b0dc20
Dani Yakir <dargomizhsky@gmail.com>
2025-07-24 06:31:51 +0300

    edited first query

--- db/queries.sql
+++ db/queries.sql
@@ -3,6 +3,6 @@
 CREATE TABLE knessets (
     id INT PRIMARY KEY,
     name TEXT NOT NULL,
-    start TIMESTAMP NOT NULL,
-    end TIMESTAMP  -- optional field, allows NULL by default
+    "start" TEXT NOT NULL,
+    "end" TEXT
 );
